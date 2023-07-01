84c591970332c5fe26117d8311055a9419399776
kevinlin <kevinlin@wisdomgarden.com>
2023-07-01 17:15:02 +0800

    feat: added kafka docker-compose and config

---
+++ money/src/main/java/com/money/money/kafka/service/KafkaConsumer.java
@@ -0,0 +1,12 @@
+package com.money.money.kafka.service;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaConsumer {
+    @KafkaListener(topics = "diary", groupId = "1")
+    public void consume(String message) {
+        System.out.println("Consumed message: " + message);
+    }
+}
